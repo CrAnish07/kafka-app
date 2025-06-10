f92ba49471273bbd71cfa2102e0bddb887fe90df
Anish Mondal <mondalanish10@gmail.com>
2025-06-10 18:27:27 +0530

    add client

--- admin.js
+++ admin.js
@@ -1,11 +1,22 @@
-const { Kafka } = require("kafkajs");
-
-const kafka = new Kafka({
-    clientId: "my-app",
-    brokers: ["https:<PRIVATE_IP>:9092"]
-});
+const { kafka } = require("./client")
 
 async function init() {
     const admin = kafka.admin();
+    console.log("Admin Connecting")
     admin.connect();
+    console.log("Admin Connected...")
+
+    console.log('Creating Topics [rider-updates]')
+    await admin.createTopics({
+        topics: [
+            {
+            topic: "rider-updates",
+            numPartitions: 2
+            },
+        ],
+    });
+    console.log('Topic Created Success [rider-updates]')
+
+    console.log("Disconnecting Admin")
+    await admin.disconnect();
 }
