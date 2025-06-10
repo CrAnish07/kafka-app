cccbc1fa5b348a920031fc29e7e4e918dad352fe
Anish Mondal <mondalanish10@gmail.com>
2025-06-10 21:42:19 +0530

    add consumer

--- producer.js
+++ producer.js
@@ -1,22 +1,34 @@
 const { kafka } = require("./client")
+const readline = require("readline")
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 async function init(){
     const producer = kafka.producer();
     await producer.connect()
     console.log("Producer Connected")
 
-    await producer.send({
-        topic: "rider-updates",
-        messages: [
-            {
-            partition: 0,
-            key: "location-updates",
-            value: JSON.stringify({name: "Vikash Kumar", loc: "SOUTH"})
-            },
-        ],
-    });
+    rl.setPrompt("> ");
+    rl.prompt();
 
-    await producer.disconnect();
+    rl.on("line", async function(line) {
+        const [ridername, location] = line.split(" ")
+        await producer.send({
+            topic: "rider-updates",
+            messages: [
+                {
+                partition: location.toLowerCase() === "north" ? 0 : 1,
+                key: "location-updates",
+                value: JSON.stringify({name: ridername, location})
+                },
+            ],
+        }); 
+    }).on("close", async () => {
+        await producer.disconnect();
+    });
 }
 
 init();
